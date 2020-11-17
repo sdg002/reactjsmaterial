a9c79402ca9634503da665a275147b18cfc05608
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-11-17 07:12:29 +0000

    Skeletal working project

---
+++ src/MyApp.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class MyApp extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world of material ui inside my App you were here</h1>          
+                <ol>
+                    <li>Nav bar with material Tabs</li>
+                    <li>Let the Navbar be the entry into all future experiments</li>
+                    <li>Use Route and switch</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+export default MyApp;           

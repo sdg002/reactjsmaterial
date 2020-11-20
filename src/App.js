c46a422bfc5f7c7a5bf7592eb780a22b36da8c8f
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-11-20 00:37:00 +0000

    Added TopNav

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import MyApp from './MyApp'
 import MyApp2 from './MyApp2'
-//import { Link } from 'react-router-dom';
+import MyApp3 from './MyApp3'
+import TopNav from './components/TopNav'
 
 function App() {
   return (
     <div>
-      <MyApp2>
-
-      </MyApp2>
+      <TopNav></TopNav>
+      <div>Body comes here</div>
     </div>
   );
 }
---
+++ src/components/TopNav.js
@@ -0,0 +1,16 @@
+import { yellow } from '@material-ui/core/colors';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class TopNav extends Component {
+    render() {
+        return (
+            <div style={{backgroundColor:"yellow", height:'5em'}}>
+                top nav comes here
+            </div>
+        );
+    }
+} 
+
+export default TopNav;  

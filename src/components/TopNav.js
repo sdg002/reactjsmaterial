0474cb8dc0db2d5f1fe02f27dfafe44f91a2b299
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-11-20 01:47:56 +0000

    Got tabs working

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,13 +1,40 @@
 import { yellow } from '@material-ui/core/colors';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 
 class TopNav extends Component {
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            value: 0
+          };
+    }
+
+    handleChange = (event, value) => {
+        //This is important - Otherwise the selection will not be shown
+        console.log(`Inside handleChange event value:${value}` )
+        this.setState({ value });
+      };
+
     render() {
         return (
             <div style={{backgroundColor:"yellow", height:'5em'}}>
-                top nav comes here
+                <AppBar position="static" color="default">
+                    <Tabs 
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        indicatorColor="primary"
+                        textColor="primary">
+                        <Tab label="Item One"   />
+                        <Tab label="Item Two"   />
+                        <Tab label="Item Three"   />
+                    </Tabs>
+                </AppBar>                
             </div>
         );
     }
